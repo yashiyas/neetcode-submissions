d5379bb5a92d4db7e7ac93661ebe2791a519e5b5
yashiyas <51291433+yashiyas@users.noreply.github.com>
2026-09-09 13:09:48 +0530

    Add: missing-element-in-sorted-array - submission-4

---
+++ Data Structures & Algorithms/missing-element-in-sorted-array/submission-4.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int missingElement(int[] nums, int k) {
+
+        List<Integer> missed = new ArrayList();
+
+        int prev = nums[0];
+
+        for(int i=0; i<nums.length; i++){
+            int missing = nums[i] - (nums[0]+i);
+            missed.add(missing);
+        }
+
+        int index = getIndex(missed,k);
+
+
+        return nums[index]+(k-missed.get(index));
+    
+        
+    }
+
+    
+
+    private int getIndex(List<Integer> missed, int target){
+        int index = 0;
+
+        int l = 0;
+        int r = missed.size()-1;
+
+        while(l <=r ){
+            int mid = (r-l)/2 + l;
+
+            if(missed.get(mid) < target){
+                index = mid;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+
+        return index;
+    }
+}
