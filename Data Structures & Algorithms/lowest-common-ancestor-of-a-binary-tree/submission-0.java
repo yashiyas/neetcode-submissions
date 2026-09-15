8fc1242f4c927b5de08716242637d8db663e9856
yashiyas <51291433+yashiyas@users.noreply.github.com>
2026-09-15 11:53:25 +0530

    Add: lowest-common-ancestor-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree/submission-0.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr == p || curr == q){
+                return curr;
+            }
+
+            if((isPresent(curr.left, p) && isPresent(curr.right, q)) || (isPresent(curr.left, q) && isPresent(curr.right, p))){
+
+                return curr;
+            }
+
+            if(isPresent(curr.left, p) && isPresent(curr.left, q)){
+                curr = curr.left;
+            }
+
+            if(isPresent(curr.right, p) && isPresent(curr.right, q)){
+                curr = curr.right;
+            }
+        }
+
+        return curr;
+        
+    }
+
+    private boolean isPresent(TreeNode curr, TreeNode p){
+        if(curr == null){
+            return false;
+        }
+        
+        if(curr == p){
+            return true;
+        }
+
+        return isPresent(curr.left, p) || isPresent(curr.right, p);
+    }
+}
